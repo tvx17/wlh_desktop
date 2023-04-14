23909e65428d4048430c14df8e88b554943ceddf
tvx17 <60501897+tvx17@users.noreply.github.com>
2023-04-14 06:40:12 +0200

    Update version.js

--- src/misc/version.js
+++ src/misc/version.js
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2022. Axel Springer SE - IT Services - Sales & Editorial Systems - Sales Service Operations
- * This source code may only be used in consultation with the Sales Service Operations department in the Sales & Editorial Content division.
- * Use outside Axel Springer SE - IT Services is not permitted.
- */
-
 export default {
   major:   0,
   minor:   0,
