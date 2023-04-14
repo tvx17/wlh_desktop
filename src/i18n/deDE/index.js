99433d1bc73f796e75f24fa4c9fc690a16211385
tvx17 <60501897+tvx17@users.noreply.github.com>
2023-04-14 06:39:20 +0200

    Update index.js

--- src/i18n/deDE/index.js
+++ src/i18n/deDE/index.js
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2022. Axel Springer SE - IT Services - Sales & Editorial Systems - Sales Service Operations
- * This source code may only be used in consultation with the Sales Service Operations department in the Sales & Editorial Content division.
- * Use outside Axel Springer SE - IT Services is not permitted.
- */
 // import {language as playerNameComponent}  from 'components/misc/playerNameComponent.deDE.i18n';
 import {language as contentsPage} from 'src/pages/contentsPage-deDE.i18n';
 
